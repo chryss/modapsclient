98b22bfb20837a048217955b19369aff26977327
Chris Waigl <chris.waigl@gmail.com>
2018-03-27 16:39:16 -0800

    Fixed up setup.py for distribution to PyPI. Crossing fingers.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='modapsclient',
@@ -9,14 +12,23 @@ setup(
     url='https://github.com/chryss/modapsclient',
     license='MIT',
     packages=['modapsclient'],
+    install_requires=[
+        'future',
+    ],
     zip_safe=False,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: GIS',
     ],
 )
